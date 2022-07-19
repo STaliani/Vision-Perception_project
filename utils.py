c7dfe13a5f31fbf9f6109954fe09507fd995ae19
Toto-0 <85567829+Toto-0@users.noreply.github.com>
2022-07-19 12:03:16 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,56 @@
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    
+    
+def visualize_model(model, num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title(f'predicted: {class_names[preds[j]]}')
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
+        
+        
+# function to convert the JavaScript object into an OpenCV image
+def js_to_image(js_reply):
+  """
+  Params:
+          js_reply: JavaScript object containing image from webcam
+  Returns:
+          img: OpenCV BGR image
+  """
+  # decode base64 image
+  image_bytes = b64decode(js_reply.split(',')[1])
+  # convert bytes to numpy array
+  jpg_as_np = np.frombuffer(image_bytes, dtype=np.uint8)
+  # decode numpy array into OpenCV BGR image
+  img = cv2.imdecode(jpg_as_np, flags=1)
+
+  return img
